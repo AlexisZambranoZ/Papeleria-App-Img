f65178fe9fd46d413f219584d46cb94ee7662692
Alexis Zambrano <zambra873@gmail.com>
2023-05-29 13:38:41 -0600

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageReducerComponent } from './image-reducer/image-reducer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageReducerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/image-reducer/image-reducer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-reducer',
+  templateUrl: './image-reducer.component.html',
+  styleUrls: ['./image-reducer.component.css']
+})
+export class ImageReducerComponent {
+  
+}
